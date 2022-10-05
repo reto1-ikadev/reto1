4ea73270564aa022831851f10549e764fda8fbf0
celia.garcia1984 <celia.garcia@ikasle.egibide.org>
2022-10-05 12:48:53 +0200

    Actualizar parada en plc

--- front/js/main.js
+++ front/js/main.js
@@ -13,11 +13,9 @@ var varAuto = 1;
 /*Esto es lo primero que se ejecuta al arrancar. Pide la posicion del tranvia al plc*/
 //Esperar a estar con el plc antes de descomentar la linea de abajo
 
-//recibirDatosIniciales(); //Recibir los Datos nada mas abrir la pagina.
+recibirDatosIniciales(); //Recibir los Datos nada mas abrir la pagina.
 
-
-
-
+/******************************BOTONES Y LISTENERS DE LA PAGINA ******************************************************* */
 /*Boton para arrancar la animacion*/
 var bGo;
 bGo = document.getElementById("rearme");
@@ -31,17 +29,20 @@ bStop.addEventListener("click", parar);
 /*ComboBox paradas. Actualizar variables de las paradas*/
 
 var selector_destino = document.getElementById("destino");
-// selector_origen.addEventListener("change", actualizarPosOrigen);º
+
 selector_destino.addEventListener("change", actualizarPosDestino);
 imgTranvia.addEventListener("animationend", listener,false);
-//imgTranvia.className = "animacion";
+
 imgTranvia.id =  "anim_tranvia1";
 
+/***SITUAR AL TRANVIA************************************************************************** */
 //actualizarPosOrigen();
 var posicionAutomatico = 0;
 var posicionAutomaticoReversa = 6;
 
-// Funcion para ocultar o mostrar el Div de los controles
+
+
+// Funcion para ocultar o mostrar el Div de los controles*********************************************/
 function ocultar(event){
     modo = " ";
     modo = event.target.value;
@@ -64,7 +65,7 @@ function ocultar(event){
         //cuadro.style.display = "block";
     }
 }
-
+/*************************************CONTROLAR LA ANIMACION TRANVIA ******************************************************* */
 function parar(){
     console.log("paro el tranvia");
     imgTranvia.style.animationPlayState = "paused" ;
@@ -75,9 +76,9 @@ function parar(){
     }
 }
 
-
  function arrancar(){
     console.log("arranco la animacion");
+    fetch("datos.html", {body: "%22app%22.estado_parada_actual=" + pos_destino+"",headers: {"Content-Type":"application/-x-www-urlencode",},method:"post"});
     /*accedo al elemento css donde están las variables y cambio el valor 
     de la variable posorigen por una de las posiciones guardadas en el array_pos_paradas */
     imgTranvia.style.animationPlayState = "running" ;
@@ -89,14 +90,13 @@ function parar(){
         }
     }
  }
-
+/**CONTROLA LOS COLORES DE LAS PARADAS ************************************************************************************ */
  function todosEnVacio() {
     for(let boton of botones) {
         boton.style.fill = "#20B2AA";
     }
 }
-
- 
+/* **CUANDO LA ANIMACIÓN TERMINA ACTUALIZA LA VARIABLE DE ORIGEN. SIN ESTO LA ANIMACIÓN VUELVE A LA PARADA DE ORIGEN**/
 function listener(event){
     switch(event.type){
         case "animationend":
@@ -115,7 +115,6 @@ function listener(event){
     el nuevo valor que se seleccione en el combo*/
 }
 
-
 function actualizarPosOrigen(){
     console.log("f(x) actualizar variables");
     /*Cambio las variables en el css para que la animacion se mueva de una parada a otra */
@@ -123,13 +122,12 @@ function actualizarPosOrigen(){
     console.log("array"+array_pos_paradas[pos_origen]);
 }
 
-
 function actualizarPosDestino(){
     pos_destino = selector_destino.options[selector_destino.selectedIndex].value;
     document.querySelector(':root').style.setProperty("--posdestino",array_pos_paradas[pos_destino-1]);
     console.log(array_pos_paradas[pos_destino-1]);
 }
-
+/*REFRESCA LA INFORMACION DE LA PAGINA, PARA QUE REINICIE EL CONTADOR DE LA ANIMACIÓN*/
 function reiniciarAnimacion(){
     /*Esta funcion la he tenido que crear para que el tranvia se vuelva a mover cada vez que pulso go. 
     Sin esto, la animación se para en una parada y no vuelve a arrancar. Solo ejecuta un ciclo*/
@@ -137,7 +135,15 @@ function reiniciarAnimacion(){
     imgTranvia.offsetHeight; /*Con esto consigo actualizar la animación*/
     imgTranvia.style.animation = null;
 }
-/** Esta funcion posiciona el tranvia en la parada que se recibe del plc. Realiza una peticion de datos y llama a la f(x) actualizarPosOrigen*/
+
+
+
+/******************************FUNCIONES DE COMUNICACION CON EL PLC ******************************************************************** */
+Function 
+
+
+
+/* Esta funcion posiciona el tranvia en la parada que se recibe del plc. Realiza una peticion de datos y llama a la f(x) actualizarPosOrigen*/
 var datosRecibidos;
 async function recibirDatosIniciales(){
     datosRecibidos = await fetch("datos.html").then((response) => response.json()).then((datos)=> {return datos } );
@@ -149,29 +155,30 @@ async function recibirDatosIniciales(){
 
 } 
 
+/*Con esta funcion pedimos datos al plc sin actualizar la posicion de origen*/
 async function recibirDatos(){
     datosRecibidos = await fetch("datos.html").then((response) => response.json()).then((datos)=> {return datos } );
-    pos_origen = estadoParadaActual();
 } 
+
+/*Devuelve si el tranvia en el plc esta arrancado o no*/
 var estadoStopGo;
 function estadoMarchaParo(){
     let estadoMarPar = datosRecibidos['marcha'];
     return estadoMarPar;
-
 }
+
+/*Devuelve la direccion que lleva el tranvia */
 var direccion;
 function estadoDireccion(){
     let estadoDir = datosRecibidos['direccion'];
     return estadoDir;
-
 }
+
+/*Devuelve la velocidad de tranvia desde el plc */
 var velocidad;
 function estadoVelocidad(){
     let estadoVel = datosRecibidos['velocidad'];
-    return estadoVel;}
-
-function automatico() {
-    arrancarAutomatico();
+    return estadoVel;
 }
 
 /*Esta funcion coge del result del fetch la parada y actualiza la parada actual*/
@@ -179,11 +186,21 @@ function estadoParadaActual(){
     let estadoParAct = datosRecibidos['parada'];
     return estadoParAct;
 }
+
+/*Esta funcion devuelve si el tranvia está parado o en movimiento desde el plc*/
 function estadoMovimiento(){
     let estadoParAct = datosRecibidos['movimiento'];
     return estadoParAct;
 }
 
+
+/*********************************************MODO AUTOMATICO*********************************************************************************** */
+/*Hay que probar si se puede llamar directamente a la funcion arrancarAutomatico sin que este dentro de otra funcion */
+/*El tranvia comienza a moverse en modo automatico */
+function automatico() {
+    arrancarAutomatico();
+}
+/*Funcion que arranca automatico*/
 async function arrancarAutomatico() {
     while (true) {
         
